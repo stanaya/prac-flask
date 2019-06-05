13add47c5920aaffd7fc772a81b585bf9eefd342
Sho Tanaya <sho.tanaya@d2c.co.jp>
2019-06-05 18:14:51 +0900

    fix

--- d2c/tokenizer/tokenizer.py
+++ d2c/tokenizer/tokenizer.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Tokenizer:
     def __init__(self):
-        self.m = MeCab.Tagger ("-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+        self.m = MeCab.Tagger ("-Owakati -d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
 
     def run(self, text):
         return self.m.parse(text)
